2fe4b56f3f74ebc7e381b18c718f47d2a0f44b6a
ibnu hasan <ibnuhasan3@gmail.com>
2026-01-16 20:41:00 +0700

    edit update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -291,6 +291,21 @@ include '../templates/sidebar.php';
                         <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                             <div class="card card-statistic-1">
                                 <div class="card-icon bg-primary">
+                                    <i class="fas fa-users"></i>
+                                </div>
+                                <div class="card-wrap">
+                                    <div class="card-header">
+                                        <h4>Total Siswa</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <?php echo $total_siswa; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1">
+                                <div class="card-icon bg-success">
                                     <i class="far fa-user"></i>
                                 </div>
                                 <div class="card-wrap">
@@ -333,6 +348,9 @@ include '../templates/sidebar.php';
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    
+                    <div class="row">
                         <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                             <div class="card card-statistic-1">
                                 <div class="card-icon bg-secondary">
@@ -344,6 +362,36 @@ include '../templates/sidebar.php';
                                     </div>
                                     <div class="card-body">
                                         <?php echo $jumlah_alpa; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1">
+                                <div class="card-icon bg-info">
+                                    <i class="fas fa-chalkboard-teacher"></i>
+                                </div>
+                                <div class="card-wrap">
+                                    <div class="card-header">
+                                        <h4>Total Guru</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <?php echo $total_guru; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1">
+                                <div class="card-icon bg-warning">
+                                    <i class="fas fa-school"></i>
+                                </div>
+                                <div class="card-wrap">
+                                    <div class="card-header">
+                                        <h4>Total Kelas</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <?php echo $total_kelas; ?>
                                     </div>
                                 </div>
                             </div>
