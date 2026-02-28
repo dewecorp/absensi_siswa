d250368476036372e372d4d374dcbb4282258c0e
dewecorp <ibnuhasan3@gmail.com>
2026-02-28 16:26:35 +0700

    update cookie

--- config/database.php
+++ config/database.php
@@ -1,4 +1,9 @@
 <?php
+// Set session lifetime to 24 hours (86400 seconds)
+ini_set('session.gc_maxlifetime', 86400);
+ini_set('session.cookie_lifetime', 86400);
+session_set_cookie_params(86400);
+
 // Database configuration
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
