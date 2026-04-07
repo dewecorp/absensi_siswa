fe05e83c3f1a2dc38d92e978aa4f1f94369542ab
dewecorp <ibnuhasan3@gmail.com>
2026-04-07 13:04:42 +0700

    update bersih

--- dump_classes.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config/database.php';
-$stmt = $pdo->query("SELECT * FROM tb_kelas");
-$classes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($classes, JSON_PRETTY_PRINT);
-unlink(__FILE__);
-?>
